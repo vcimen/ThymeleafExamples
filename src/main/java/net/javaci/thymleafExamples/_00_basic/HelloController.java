3e7165a2376dbd58ce98d3aa0398c47691821778
vcimen <vcimen@na.edu>
2021-02-04 09:37:51 -0500

    spring MVC Thymeleaf

---
+++ src/main/java/net/javaci/thymleafExamples/_00_basic/HelloController.java
@@ -0,0 +1,34 @@
+package net.javaci.thymleafExamples._00_basic;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+
+public class HelloController {
+	
+	@GetMapping("merhaba")
+	
+	
+	public String renderHello(Model model) {
+		model.addAttribute("welcome_message","Merhaba Arkadaslar");
+	
+		
+		return "hello";
+		
+		
+	}
+@GetMapping("merhaba2")
+	
+	
+public String renderHello2(Model model, @RequestParam(name = "name", required = false, defaultValue = "World")
+ String name) {
+	model.addAttribute("name",name);
+		
+		return "hello2";
+		
+		
+	}
+
+}
